44200497441c8088a2f28d575b8e30381623b54f
BrandonKutivan <bskutivan@gmail.com>
2020-04-11 19:04:48 -0700

    implemented the same code for times

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,12 @@ $('#currentDay').append(currentDate);
 eventsRow1 = [];
 eventsRow2 = [];
 eventsRow3 = [];
+eventsRow4 = [];
+eventsRow5 = [];
+eventsRow6 = [];
+eventsRow7 = [];
+eventsRow8 = [];
+eventsRow9 = [];
 
 //********* Save Button Scripts***********
 $("#row-1").on("click", "#saveBtn-1", function(event){
@@ -44,6 +50,71 @@ $("#row-3").on("click", "#saveBtn-3", function(event){
     localStorage.setItem("10amEvents", JSON.stringify(eventsRow3));
 });
 
+$("#row-4").on("click", "#saveBtn-4", function(event){
+    event.preventDefault();
+    var event4 = $("#txtArea-4")
+    .val()
+    .trim()
+
+    eventsRow4.push(event4);
+
+    localStorage.setItem("11amEvents", JSON.stringify(eventsRow4));
+});
+
+$("#row-5").on("click", "#saveBtn-5", function(event){
+    event.preventDefault();
+    var event5 = $("#txtArea-5")
+    .val()
+    .trim()
+
+    eventsRow5.push(event5);
+
+    localStorage.setItem("12pmEvents", JSON.stringify(eventsRow5));
+});
+
+$("#row-6").on("click", "#saveBtn-6", function(event){
+    event.preventDefault();
+    var event6 = $("#txtArea-6")
+    .val()
+    .trim()
+
+    eventsRow6.push(event6);
+
+    localStorage.setItem("1pmEvents", JSON.stringify(eventsRow6));
+});
+
+$("#row-7").on("click", "#saveBtn-7", function(event){
+    event.preventDefault();
+    var event7 = $("#txtArea-7")
+    .val()
+    .trim()
+
+    eventsRow7.push(event7);
+
+    localStorage.setItem("1pmEvents", JSON.stringify(eventsRow7));
+});
+
+$("#row-8").on("click", "#saveBtn-8", function(event){
+    event.preventDefault();
+    var event8 = $("#txtArea-8")
+    .val()
+    .trim()
+
+    eventsRow8.push(event8);
+
+    localStorage.setItem("1pmEvents", JSON.stringify(eventsRow8));
+});
+
+$("#row-9").on("click", "#saveBtn-9", function(event){
+    event.preventDefault();
+    var event9 = $("#txtArea-9")
+    .val()
+    .trim()
+
+    eventsRow9.push(event9);
+
+    localStorage.setItem("1pmEvents", JSON.stringify(eventsRow9));
+});
 
 
 // Load LocalStorage values
@@ -51,3 +122,9 @@ $("#row-3").on("click", "#saveBtn-3", function(event){
 $("#txtArea-1").val(JSON.parse(localStorage.getItem("9amEvents")));
 $("#txtArea-2").val(JSON.parse(localStorage.getItem("10amEvents")));
 $("#txtArea-3").val(JSON.parse(localStorage.getItem("11amEvents")));
+$("#txtArea-4").val(JSON.parse(localStorage.getItem("12pmEvents")));
+$("#txtArea-5").val(JSON.parse(localStorage.getItem("1pmEvents")));
+$("#txtArea-6").val(JSON.parse(localStorage.getItem("2pmEvents")));
+$("#txtArea-7").val(JSON.parse(localStorage.getItem("3pmEvents")));
+$("#txtArea-8").val(JSON.parse(localStorage.getItem("4pmEvents")));
+$("#txtArea-9").val(JSON.parse(localStorage.getItem("5pmEvents")));
