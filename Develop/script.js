0349b40a3e8ec26de204d43b8309145188daa9e3
BrandonKutivan <bskutivan@gmail.com>
2020-04-11 18:18:43 -0700

    captured current date with as moment variable. and formated for top of page.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,14 @@
+// Current Date displayed at top of page
+
+var currentDateEl = $('#currentDay');
+
+var currentDate = moment().format('dddd [,] MMMM Do');
+console.log(currentDate);
+$('#currentDay').append(currentDate);
+
+
 // target event div and convert to input on click
+
 
 
 // on click of save button conert input back to original element
